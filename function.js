40d789643a95d6e229a57e77aa7db1f3fa15b33a
javaid05 <javaidmalik.khan@gmail.com>
2021-11-01 15:08:24 +0000

    exercise2

--- function.js
+++ function.js
@@ -16,3 +16,13 @@ const dice = function () {
   return Math.ceil(Math.random() * 6);
 };
 console.log(dice());
+
+/* EXERCISE 2
+    Write a function called whoIsBigger which receives 2 numbers as parameters and returns the biggest one.
+*/
+printNewLine("Exercise 2");
+const whoIsBigger = function (num1, num2) {
+  if (num1 > num2) return num1;
+  else return num2;
+};
+console.log(whoIsBigger(40, 30));
