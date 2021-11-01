d0ee1c09d1c9cd01c2fe3931d0bddf7769b58cb8
javaid05 <javaidmalik.khan@gmail.com>
2021-11-01 16:55:11 +0000

    array-object

---
+++ array-object.js
@@ -0,0 +1,14 @@
+// JS Arrays & Objects
+// NOTE: the movies array used in some exercises is defined at the end of this file
+const printNewLine = function (question) {
+  console.log(
+    `\n<--------------------Answer JS Function ${question}----------------->\n\n`
+  );
+};
+
+printNewLine("Exercise 11");
+
+/* EXERCISE 11
+   Write a function called deleteProp which receives an object and a string as parameters,
+   and returns the given object after deleting its property named as the given string.
+*/
