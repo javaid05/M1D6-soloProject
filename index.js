244528d62bea34b3d5e3ab28fff8b268af591d04
javaid05 <javaidmalik.khan@gmail.com>
2021-11-01 13:52:36 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+/*
+ASSIGNMENT RULES
+- All the answers must be written in JavaScript
+- You can ask for help, reach the Teaching Assistants if needed
+- You can Google / use StackOverflow BUT only when you think you need something we didn't cover during lessons yet
+- You can test your code in a separate file or de-commenting the single exercises in this one.
+- You can use the bash terminal, the VSCode terminal or the one embedded in your Operating System if you're using macOS or Linux.
+- The solution must be pushed to the repository and be available for the tutors by the end of the day (5PM CET)
+- Complete as many exercises as you can
+*/
+
+// JS Basics
+
+/* EXERCISE A
+   Create a variable called test and assign a string value to it.
+*/
+const printNewLine = function (question) {
+  console.log(
+    `\n<--------------------Answer ${question}----------------->\n\n`
+  );
+};
+printNewLine("Excercise A");
+
+let test = "test";
+console.log(test);
